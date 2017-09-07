ebcc2acb2f606f65301c7d89ff75f4d36d107dc7
Sujip Thapa <sudiptpa@gmail.com>
2017-09-07 22:15:40 +0545

    Added basic functionality for middleware

--- src/config/domain.php
+++ src/config/domain.php
@@ -1,5 +1,16 @@
 <?php
 
-return [
+/*
+|--------------------------------------------------------------------------
+| Set your preferred domain (www or non-www)
+|--------------------------------------------------------------------------
+|
+| Below set the true or false boolean value  in `www` for the application
+| which may be used set the host before or after a request into your
+| application.
+|
+ */
 
+return [
+    'www' => false,
 ];
